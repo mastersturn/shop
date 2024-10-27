c1f7f070b8d76293d28c9315203aae1c70aa8843
Михаил Сергеев <sergeev@i5.solutions>
2024-10-27 18:12:35 +0600

    SHOP-styled: Новый дизайн

--- myshop/orders/forms.py
+++ myshop/orders/forms.py
@@ -6,3 +6,11 @@ class OrderCreateForm(forms.ModelForm):
     class Meta:
         model = Order
         fields = ['first_name', 'last_name', 'email', 'address', 'postal_code', 'city']
+        widgets = {
+                    'first_name': forms.TextInput(attrs={'class': 'form-input'}),
+                    'last_name': forms.TextInput(attrs={'class': 'form-input'}),
+                    'email': forms.EmailInput(attrs={'class': 'form-input'}),
+                    'address': forms.TextInput(attrs={'class': 'form-input'}),
+                    'postal_code': forms.TextInput(attrs={'class': 'form-input'}),
+                    'city': forms.TextInput(attrs={'class': 'form-input'}),
+                }
